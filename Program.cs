fbc5d16fc2c23f19d18648a2fa901cc44ca4a514
sosinatolossa <sosinatolossa16@gmail.com>
2021-03-01 15:27:30 -0600

    phase 2 complete

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
 using System;
 
+string secretNumber = "72";
+
 //we will display a message to the user asking them to guess the secret number
 string SecretQuestion = "Guess the secret number";
 Console.Write($"{SecretQuestion}: ");
 //we will save the user's guess as input and save it as a variable
 string answer = Console.ReadLine();
-Console.Write(answer);
+
+if (answer != secretNumber) 
+{
+    Console.Write($"You guessed wrong!! Muahahaha, maybe next time pal.");
+}
+else
+{
+    Console.Write("Oo, you're a good one. Great job! You guessed it right!");
+}
