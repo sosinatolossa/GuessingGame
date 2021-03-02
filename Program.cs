37e09e236589880ed516f80703400e360d2eb7a1
sosinatolossa <sosinatolossa16@gmail.com>
2021-03-02 11:42:22 -0600

    phase 7 complete

--- Program.cs
+++ Program.cs
@@ -6,43 +6,128 @@ Console.WriteLine(secretNumber);
 //we will display a message to the user asking them to guess the secret number
 string SecretQuestion = "Guess the secret number";
 
-int num = 0;
+int numOfTriesForEasyLevel = 0;
+int numOfTriesForMediumLevel = 0;
+int numOfTriesForDifficultLevel = 0;
 int currentGuess = 1;
-int guessesLeft = 4;
+int guessesForDifficultLevel = 4;
+int guessesForMediumLevel = 6;
+int guessesForEasyLevel = 8;
 
-while (num < 4)
+string questionOfDifficultyLevel = "How hard do you want this game to be?";
+Console.WriteLine($"{questionOfDifficultyLevel} Easy/Medium/Hard: ");
+string userDifficultyLevelAnswer = Console.ReadLine().ToLower();
+
+if (userDifficultyLevelAnswer == "easy")
 {
-
-    Console.Write($"{SecretQuestion}: ");
-    //we will save the user's guess as input and save it as a variable
-    string answer = Console.ReadLine();
-    int parsedAnswer = Int32.Parse(answer);
-    //if the user's answer is not same as our secret number
-    if (parsedAnswer != secretNumber)
+    while (numOfTriesForEasyLevel < guessesForEasyLevel)
     {
-        //print out this mssage
-        Console.WriteLine($"You guessed wrong!! Muahahaha, maybe next time pal.");
+        Console.Write($"{SecretQuestion}: ");
+        //we will save the user's guess as input and save it as a variable
+        string answer = Console.ReadLine();
+        int parsedAnswer = Int32.Parse(answer);
+        //if the user's answer is not same as our secret number
+        if (parsedAnswer != secretNumber)
+        {
+            //print out this mssage
+            Console.WriteLine($"You guessed wrong!! Muahahaha, maybe next time pal.");
 
-        if (parsedAnswer > secretNumber)
-        {
-            Console.WriteLine("But your guess is too high. Try to go lower.");
+            if (parsedAnswer > secretNumber)
+            {
+                Console.WriteLine("But your guess is too high. Try to go lower.");
+            }
+            else
+            {
+                Console.WriteLine("But your guess is too low. Try to go up.");
+            }
         }
-        else
+        else //otherwise
         {
-            Console.WriteLine("But your guess is too low. Try to go up.");
+            //tell them this
+            Console.WriteLine("Oo, you're a good one. Great job! You guessed it right!");
+            break;
         }
+        guessesForEasyLevel--;
+
+        Console.WriteLine($"Your guess ({currentGuess})");
+        Console.WriteLine($"You have {guessesForEasyLevel} guesses left but no pressure.");
+        currentGuess++;
+
     }
-    else //otherwise
+}
+
+else if (userDifficultyLevelAnswer == "medium")
+{
+    while (numOfTriesForMediumLevel < guessesForMediumLevel)
     {
-        //tell them this
-        Console.WriteLine("Oo, you're a good one. Great job! You guessed it right!");
-        break;
+        Console.Write($"{SecretQuestion}: ");
+        //we will save the user's guess as input and save it as a variable
+        string answer = Console.ReadLine();
+        int parsedAnswer = Int32.Parse(answer);
+        //if the user's answer is not same as our secret number
+        if (parsedAnswer != secretNumber)
+        {
+            //print out this mssage
+            Console.WriteLine($"You guessed wrong!! Muahahaha, maybe next time pal.");
+
+            if (parsedAnswer > secretNumber)
+            {
+                Console.WriteLine("But your guess is too high. Try to go lower.");
+            }
+            else
+            {
+                Console.WriteLine("But your guess is too low. Try to go up.");
+            }
+        }
+        else //otherwise
+        {
+            //tell them this
+            Console.WriteLine("Oo, you're a good one. Great job! You guessed it right!");
+            break;
+        }
+        guessesForMediumLevel--;
+
+        Console.WriteLine($"Your guess ({currentGuess})");
+        Console.WriteLine($"You have {guessesForMediumLevel} guesses left but no pressure.");
+        currentGuess++;
+
     }
-    guessesLeft--;
+}
 
-    Console.WriteLine($"Your guess ({currentGuess})");
-    Console.WriteLine($"You have {guessesLeft} guesses left but no pressure.");
-    currentGuess++;
-    num++;
+else if (userDifficultyLevelAnswer == "hard")
+{
+    while (numOfTriesForDifficultLevel < guessesForDifficultLevel)
+    {
+        Console.Write($"{SecretQuestion}: ");
+        //we will save the user's guess as input and save it as a variable
+        string answer = Console.ReadLine();
+        int parsedAnswer = Int32.Parse(answer);
+        //if the user's answer is not same as our secret number
+        if (parsedAnswer != secretNumber)
+        {
+            //print out this mssage
+            Console.WriteLine($"You guessed wrong!! Muahahaha, maybe next time pal.");
 
+            if (parsedAnswer > secretNumber)
+            {
+                Console.WriteLine("But your guess is too high. Try to go lower.");
+            }
+            else
+            {
+                Console.WriteLine("But your guess is too low. Try to go up.");
+            }
+        }
+        else //otherwise
+        {
+            //tell them this
+            Console.WriteLine("Oo, you're a good one. Great job! You guessed it right!");
+            break;
+        }
+        guessesForDifficultLevel--;
+
+        Console.WriteLine($"Your guess ({currentGuess})");
+        Console.WriteLine($"You have {guessesForDifficultLevel} guesses left but no pressure.");
+        currentGuess++;
+
+    }
 }
