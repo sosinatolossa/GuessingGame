6eb002a2a3fa199fa2558f157a8ed1a82905de39
sosinatolossa <sosinatolossa16@gmail.com>
2021-03-02 10:08:56 -0600

    phase 5 complete

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
 using System;
 
-string secretNumber = "72";
+int secretNumber = new Random().Next(1, 100);
+//Console.WriteLine(secretNumber);
 
 //we will display a message to the user asking them to guess the secret number
 string SecretQuestion = "Guess the secret number";
 
 int num = 0;
 int currentGuess = 1;
+int guessesLeft = 4;
 
 while (num < 4)
 {
@@ -14,8 +16,9 @@ while (num < 4)
     Console.Write($"{SecretQuestion}: ");
     //we will save the user's guess as input and save it as a variable
     string answer = Console.ReadLine();
+    int parsedAnswer = Int32.Parse(answer);
     //if the user's answer is not same as our secret number
-    if (answer != secretNumber)
+    if (parsedAnswer != secretNumber)
     {
         //print out this mssage
         Console.WriteLine($"You guessed wrong!! Muahahaha, maybe next time pal.");
@@ -26,7 +29,10 @@ while (num < 4)
         Console.WriteLine("Oo, you're a good one. Great job! You guessed it right!");
         break;
     }
+    guessesLeft--;
+
     Console.WriteLine($"Your guess ({currentGuess})");
+    Console.WriteLine($"You have {guessesLeft} guesses left but no pressure.");
     currentGuess++;
     num++;
 }
